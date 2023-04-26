c18566c5769a3547657c5680acebe61d7544cfbe
IlyaShpalochkin <4frankyyoung@gmail.com>
2023-04-26 20:55:45 +0400

    github actions run tests

--- src/test/java/chromeDriver/DriverManager.java
+++ src/test/java/chromeDriver/DriverManager.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
     static {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir"));
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/chromedriver");
     }
 
     public static ChromeDriver getChromeDriver() {
