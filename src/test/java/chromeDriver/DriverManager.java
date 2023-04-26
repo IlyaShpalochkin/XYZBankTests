cf81445fecbf2e505113cc6d042152c3f741921e
IlyaShpalochkin <4frankyyoung@gmail.com>
2023-04-26 21:12:47 +0400

    github actions run tests

--- src/test/java/chromeDriver/DriverManager.java
+++ src/test/java/chromeDriver/DriverManager.java
@@ -1,17 +1,15 @@
 package chromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
     static {
-
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/chromedriverr/chromedriverr");
     }
 
     public static ChromeDriver getChromeDriver() {
-        ChromeDriverManager.getInstance().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         return new ChromeDriver(options);
